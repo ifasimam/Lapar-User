d1a6d7731f3158d4fb0e6c0a8cce7fedd6670cf7
olin48 <olin4all@gmail.com>
2017-02-13 00:03:21 +0700

    First

---
+++ app/src/main/java/com/panelic/laparuser/fragment/TabMainFrag.java
@@ -0,0 +1,34 @@
+package com.panelic.laparuser.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.panelic.laparuser.R;
+
+/**
+ * Created by Sholihin on 12/02/2017.
+ * Developer Team : Panelic Studio
+ */
+
+public class TabMainFrag extends Fragment {
+
+    public TabMainFrag() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
